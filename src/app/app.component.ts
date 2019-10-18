9eae5963ea0a66356e65d76be39e1e1f2400b1f9
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-10-18 10:28:37 +0200

    Render basic map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+
+  options = {
+    layers: [
+      tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {maxZoom: 19, attribution: '&copy; OpenStreetMap contributors'}
+      )
+    ],
+    zoom: 11,
+    center: latLng([38.088, 13.155])
+  };
+
+ }
